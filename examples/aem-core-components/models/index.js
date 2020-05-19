b5b940f6b692d20dc0ae8846e21884667ec3a2bd
James Pica <jamespica11@gmail.com>
2020-05-19 19:51:30 -0400

    Add Breadcrumb and Button core components as js models

--- examples/aem-core-components/models/index.js
+++ examples/aem-core-components/models/index.js
@@ -3,4 +3,5 @@ module.exports = {
   'com.adobe.cq.wcm.core.components.models.Accordion': require('./com.adobe.cq.wcm.core.components.models.Accordion'),
   'com.adobe.cq.wcm.core.components.models.Text': require('./com.adobe.cq.wcm.core.components.models.Text'),
   'com.adobe.cq.wcm.core.components.models.List': require('./com.adobe.cq.wcm.core.components.models.List'),
+  'com.adobe.cq.wcm.core.components.models.Button': require('./com.adobe.cq.wcm.core.components.models.Button'),
 };
